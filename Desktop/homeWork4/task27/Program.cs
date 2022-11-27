8fd6df8d6b5b1e59cd0ea3bde64a0e96fa011c85
badanin5@gmail.com <badanin5@gmail.com>
2022-11-27 21:02:29 +0300

    Task 27

---
+++ Desktop/homeWork4/task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine ("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите число: "+ GetSumNum(num1));
+
+int GetSumNum (int res)
+{
+    int sum = 0;
+    while (res>0)
+    {
+        sum += res%10;
+        res = res/10;
+    }
+    return sum;
+}
