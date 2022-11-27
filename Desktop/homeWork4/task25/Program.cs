b0ee4dc8f37873c02da3945bb203bc639ae536bf
badanin5@gmail.com <badanin5@gmail.com>
2022-11-27 20:52:48 +0300

    Task 25

---
+++ Desktop/homeWork4/task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит 
+// число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine ("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int rate = GetRateNum(num1, num2);
+Console.WriteLine (rate);
+
+int GetRateNum(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result *=a;
+    }
+    return result;
+
+}
