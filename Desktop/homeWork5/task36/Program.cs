f835be931a637c83ddf56c6fd4f80d46fba747e3
badanin5@gmail.com <badanin5@gmail.com>
2022-11-27 22:02:37 +0300

    task36

---
+++ Desktop/homeWork5/task36/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+RandomArray (array);
+Console.WriteLine("Массив: ");
+PrintArray (array);
+
+int sum = 0;
+
+for (int i = 0; i < array.Length; i=i+2) //i+=2)
+{
+    sum += array[i];
+}
+
+Console.WriteLine($"всего {array.Length} чисел, сумма элементов {sum}");
+
+void RandomArray (int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,100);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
