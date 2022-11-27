ef24877495fcd8ebfcca170137b9f9109c5cd336
badanin5@gmail.com <badanin5@gmail.com>
2022-11-27 17:36:57 +0300

    Task 13

---
+++ Desktop/homework2/tusk13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine ("Введите чило: ");
+
+string str = Console.ReadLine();
+int num = Convert.ToInt32(str);
+
+if (num<100)
+{
+    Console.WriteLine ("Третьего числа нет");
+}
+else
+{
+    Console.WriteLine (str[2]);
+}
